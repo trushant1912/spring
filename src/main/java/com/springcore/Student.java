e4ef87a83d5f16cefc21f1ac6ddf092c643076d2
Trushant Ramdas Jadhav <trushantramdasjadhav@Trushants-MacBook-Air.local>
2024-05-08 21:23:44 +0530

    first commit

---
+++ src/main/java/com/springcore/Student.java
@@ -0,0 +1,44 @@
+package com.springcore;
+
+public class Student {
+	
+	private int  studentId;
+	private String studentName;
+	private String studentCity;
+	
+	public Student() {
+		super();
+		
+	}
+	
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentCity() {
+		return studentCity;
+	}
+	public void setStudentCity(String studentCity) {
+		this.studentCity = studentCity;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentCity=" + studentCity
+				+ "]";
+	}
+	
+	
+	
+	
+
+}
